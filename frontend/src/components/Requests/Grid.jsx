8bf466d24b7a4b21d9b13f21347b60b1d3318c0d
Camilo Londoño <cl.mon9566@gmail.com>
2023-05-06 17:42:11 -0500

    fix requests grid.

--- frontend/src/components/Requests/Grid.jsx
+++ frontend/src/components/Requests/Grid.jsx
@@ -18,60 +18,58 @@ const GridRequests = () => {
     fetchData();
   }, []);
 
-  if (requests.length > 0) {
-    return (
-      <div className='flex flex-col items-center justify-center h-screen'>
-        <table className={tableStyles}>
-          <caption className='caption-top pb-4 text-3xl'>Requests</caption>
-          <caption className='caption-bottom pt-4'>
-            <div className='flex justify-around'>
-              <button
-                onClick={() => navigate('/')}
-                className='text-sm bg-blue-500 hover:bg-blue-700 p-3 rounded-md font-bold text-white w-full'
-              >
-                Back
-              </button>
-              <button
-                onClick={() => navigate('/requests/create')}
-                className='text-sm bg-green-500 hover:bg-green-700 p-3 rounded-md font-bold text-white w-full'
-              >
-                New Request
-              </button>
-            </div>
-          </caption>
-          <thead>
-            <tr>
-              <th className={thStyles}>#</th>
-              <th className={thStyles}>Code</th>
-              <th className={thStyles}>Description</th>
-              <th className={thStyles}>Summary</th>
-              <th className={thStyles}>Employee Name</th>
-              <th className={thStyles}></th>
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <table className={tableStyles}>
+        <caption className='caption-top pb-4 text-3xl'>Requests</caption>
+        <caption className='caption-bottom pt-4'>
+          <div className='flex justify-around'>
+            <button
+              onClick={() => navigate('/')}
+              className='text-sm bg-blue-500 hover:bg-blue-700 p-3 rounded-md font-bold text-white w-full'
+            >
+              Back
+            </button>
+            <button
+              onClick={() => navigate('/requests/create')}
+              className='text-sm bg-green-500 hover:bg-green-700 p-3 rounded-md font-bold text-white w-full'
+            >
+              New Request
+            </button>
+          </div>
+        </caption>
+        <thead>
+          <tr>
+            <th className={thStyles}>#</th>
+            <th className={thStyles}>Code</th>
+            <th className={thStyles}>Description</th>
+            <th className={thStyles}>Summary</th>
+            <th className={thStyles}>Employee Name</th>
+            <th className={thStyles}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {requests.map((request, index) => (
+            <tr key={index}>
+              <td className={tdStyles}>{index + 1}</td>
+              <td className={tdStyles}>{request.code}</td>
+              <td className={tdStyles}>{request.description}</td>
+              <td className={tdStyles}>{request.summary}</td>
+              <td className={tdStyles}>{request.employee_name}</td>
+              <td className={tdStyles}>
+                <button
+                  className='p-1 bg-red-500 text-white text-xs'
+                  onClick={() => navigate(`/requests/delete/${request.id}`)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {requests.map((request, index) => (
-              <tr key={index}>
-                <td className={tdStyles}>{index + 1}</td>
-                <td className={tdStyles}>{request.code}</td>
-                <td className={tdStyles}>{request.description}</td>
-                <td className={tdStyles}>{request.summary}</td>
-                <td className={tdStyles}>{request.employee_name}</td>
-                <td className={tdStyles}>
-                  <button
-                    className='p-1 bg-red-500 text-white text-xs'
-                    onClick={() => navigate(`/requests/delete/${request.id}`)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default GridRequests;
